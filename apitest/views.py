5e5f23c1116e4dcbc04b5a38a2832c9fcfee8ea1
ericl <ericwh08@gmail.com>
2019-10-15 22:21:16 +0800

    master

--- apitest/views.py
+++ apitest/views.py
@@ -61,4 +61,13 @@ def test_report(request):
     username = request.session.get('user','')
     apis_list = Apis.objects.all()
     apis_count = Apis.objects.all().count()
-    db = pymysql.connect(user='root', db='autotest', passwd='test123456')
+    db = pymysql.connect(user='root', db='autotest', passwd='test123456',host='127.0.0.1')
+    cursor = db.cursor()
+    sql1 = 'SELECT count(id) FROM apitest_apis WHERE apitest_apis.apistatus=1'
+    aa=cursor.execute(sql1)
+    apis_pass_count = [row[0] for row in cursor.fetchmany(aa)][0]
+    sql2= 'SELECT count(id) FROM apitest_apis WHERE apitest_apis.apistatus=0'
+    bb=cursor.execute(sql2)
+    apis_fail_count = [row[0] for row in cursor.fetchmany(bb)][0]
+    db.close()
+    return render(request,"report.html", {"user": username,"apiss": apis_list,"apiscounts": apis_count,"apis_pass_counts": apis_pass_count,"apis_fail_counts": apis_fail_count})
